22a00a8ed12567c79c851470b3a952377f01913c
Muteshi <lumteshi@gmail.com>
2021-01-27 14:59:13 +0300

    Setup websocket

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const http = require("http");
 const chalk = require("chalk");
 const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketio(server);
 
+const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on("connection", () => {
+  console.log(chalk.bold.green("New webSocket connection.."));
+});
+
+server.listen(port, () => {
   console.log(chalk.bold.green("Server is running on port " + port));
 });
